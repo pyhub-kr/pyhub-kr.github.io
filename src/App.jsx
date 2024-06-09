cae17b791db04f453644c12671ade86859b0112c
Chinseok Lee <me@pyhub.kr>
2024-06-09 23:02:10 +0900

    daisyui 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">파이썬 사랑방</h1>
+    <div className="text-center my-5">
+      <h1 className="text-3xl font-bold">파이썬사랑방 AI 도구</h1>
+      <p className="my-5">
+        파이썬사랑방 AI 도구는 파이썬사랑방에서 제공하는 AI 도구를 사용할 수
+        있는 웹 애플리케이션입니다.
+      </p>
     </div>
   );
 }
